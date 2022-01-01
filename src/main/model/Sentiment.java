b3c01b5311124583d71fe93ed4f1ca706a22fbc8
patrickhong21 <95744076+patrickhong21@users.noreply.github.com>
2021-12-31 22:17:31 -0800

    Sentiment constructor

--- src/main/model/Sentiment.java
+++ src/main/model/Sentiment.java
@@ -4,4 +4,13 @@ public class Sentiment {
     private String heading;
     private Double sentimentScore;
     private String source;
+
+    public Sentiment(String heading, Double sentimentScore, String source) {
+        this.heading = heading;
+        this.sentimentScore = sentimentScore;
+        this.source = source;
+    }
+
+
+
 }
