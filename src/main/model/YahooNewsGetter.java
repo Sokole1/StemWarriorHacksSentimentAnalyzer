f6d92e0dc3170679a8dd10abe3a7f6c6bbb2962d
William <william@gameofapps.org>
2022-01-01 11:06:20 -0800

    Read api keys from gitignored. config.json file

--- src/main/model/YahooNewsGetter.java
+++ src/main/model/YahooNewsGetter.java
@@ -3,11 +3,14 @@ package main.model;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class YahooNewsGetter implements NewsGetter {
 
@@ -16,20 +19,22 @@ public class YahooNewsGetter implements NewsGetter {
         return getNewsTurnIntoSentiments(getRawNews(ticker));
     }
 
+    // TODO: remove this function when testing not needed
     public static void main(String[] args) {
 
         YahooNewsGetter yahooNewsGetter = new YahooNewsGetter();
-//        String r = getRawNews("AAPL");
-        Sentiment[] arr = yahooNewsGetter.getNewsSentiment("AAPL");
-
-        //change index from 0-99 for testing
-        System.out.println(arr[50].displayHeadingAndSource());
+        yahooNewsGetter.getYahooApiKey();
+////        String r = getRawNews("AAPL");
+//        Sentiment[] arr = yahooNewsGetter.getNewsSentiment("AAPL");
+//
+//        //change index from 0-99 for testing
+//        System.out.println(arr[50].displayHeadingAndSource());
     }
 
     private String getRawNews(String ticker) {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://yfapi.net/ws/insights/v1/finance/insights?symbol=" + ticker))
-                .header("x-api-key", "zQ0HGB2hvf84gDCfuvw5X3KoVrIVlX1b43lIaEgy")
+                .header("x-api-key", getYahooApiKey())
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = null;
@@ -63,6 +68,17 @@ public class YahooNewsGetter implements NewsGetter {
         return sentiments;
     }
 
+    private String getYahooApiKey() {
+        File file = new File("config.json");
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
+            JSONObject json = new JSONObject(content);
+            return json.getString("yahooApiKey");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.getLocalizedMessage();
+        }
+    }
 }
 
 
