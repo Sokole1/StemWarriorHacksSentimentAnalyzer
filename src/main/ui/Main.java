1dfd4df7afc5db6c011b7afa1168cb5eb56e182f
William <william@gameofapps.org>
2022-01-02 09:23:15 -0800

    Modified Sentiment to take optional url. GoogleNewsGetter now sets sentiment url

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,17 +1,25 @@
 package main.ui;
 
+import main.model.*;
+
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 //        NewsGetter newsGetter = new YahooNewsGetter();
-//        StockInfoGetter stockInfoGetter = new YahooStockInfoGetter();
-//        SentimentGetter sentimentGetter = new SymblSentimentGetter();
+        StockInfoGetter stockInfoGetter = new YahooStockInfoGetter();
+        SentimentGetter sentimentGetter = new SymblSentimentGetter();
 //        Handler handler = new Handler(stockInfoGetter, newsGetter, sentimentGetter);
 //        System.out.println(Arrays.toString(handler.initializeFavouriteStocks()));
 //        Stock myStock = handler.setUpStock("AAPL");
 //        System.out.println(myStock);
-        StockDisplay stockDisplay = new StockDisplay();
+
+//        StockDisplay stockDisplay = new StockDisplay();
+        NewsGetter googleNewsGetter = new GoogleNewsGetter();
+        System.out.println(Arrays.toString(googleNewsGetter.getNewsSentiment("TSLA")));
+//        Handler handler = new Handler(stockInfoGetter, googleNewsGetter, sentimentGetter);
+//        System.out.println(handler.setUpStock("GME"));
     }
 }
