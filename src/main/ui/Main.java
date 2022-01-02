2fc09249fcebc9e865a469d0caf0379fcae0e926
William <william@gameofapps.org>
2022-01-01 23:07:16 -0800

    Began StockDisplay UI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,19 +1,17 @@
 package main.ui;
 
-import main.model.*;
-
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        NewsGetter newsGetter = new YahooNewsGetter();
-        StockInfoGetter stockInfoGetter = new YahooStockInfoGetter();
-        SentimentGetter sentimentGetter = new SymblSentimentGetter();
-        Handler handler = new Handler(stockInfoGetter, newsGetter, sentimentGetter);
-        System.out.println(Arrays.toString(handler.initializeFavouriteStocks()));
+//        NewsGetter newsGetter = new YahooNewsGetter();
+//        StockInfoGetter stockInfoGetter = new YahooStockInfoGetter();
+//        SentimentGetter sentimentGetter = new SymblSentimentGetter();
+//        Handler handler = new Handler(stockInfoGetter, newsGetter, sentimentGetter);
+//        System.out.println(Arrays.toString(handler.initializeFavouriteStocks()));
 //        Stock myStock = handler.setUpStock("AAPL");
 //        System.out.println(myStock);
+        StockDisplay stockDisplay = new StockDisplay();
     }
 }
